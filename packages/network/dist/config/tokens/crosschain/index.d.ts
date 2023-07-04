457591c728b5296036a6a7f2b8a5b1614c2be74c
Alex D <pr0fedt@gmail.com>
2023-07-04 13:53:25 +0200

    fix interlay

--- packages/network/dist/config/tokens/crosschain/index.d.ts
+++ packages/network/dist/config/tokens/crosschain/index.d.ts
@@ -1,11 +1,13 @@
 import * as astar from "./astar";
 import * as genshiro from "./genshiro";
 import * as interlay from "./interlay";
+import * as parallel from "./parallel";
 import * as statemint from "./statemint";
 declare const _default: {
     readonly astar: typeof astar;
     readonly genshiro: typeof genshiro;
     readonly interlay: typeof interlay;
+    readonly parallel: typeof parallel;
     readonly statemint: typeof statemint;
 };
 export default _default;
--- packages/network/src/config/tokens/crosschain/index.ts
+++ packages/network/src/config/tokens/crosschain/index.ts
@@ -1,11 +1,13 @@
 import * as astar from "./astar";
 import * as genshiro from "./genshiro";
 import * as interlay from "./interlay";
+import * as parallel from "./parallel";
 import * as statemint from "./statemint";
 
 export default {
   astar,
   genshiro,
   interlay,
+  parallel,
   statemint,
 } as const;
