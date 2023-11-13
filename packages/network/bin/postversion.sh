397b1e082e9b817cc3d15b0b8e4acfefb54dc9ed
Alex D <pr0fedt@gmail.com>
2023-11-13 13:12:18 +0100

    publish public not restricted

--- packages/network/bin/postversion.sh
+++ packages/network/bin/postversion.sh
@@ -3,7 +3,7 @@ SCRIPT_RELATIVE_PATH=`dirname "$0"`
 PROJECT_PATH=`( cd "$SCRIPT_RELATIVE_PATH/.." && pwd )`
 git push --tags
 if yarn publish . \
---access restricted \
+--access public \
 --new-version $npm_package_version \
 --tag latest; then
   echo "version $npm_package_version published; do not forget to push your changes to repository"
