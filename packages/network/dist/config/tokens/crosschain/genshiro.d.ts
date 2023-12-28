9aa1cdc12796a11bbec221b4bc3aeb04603bb91b
Dmitry Egorov <d.yegorov@gmail.com>
2023-12-28 15:21:29 +0300

    feat: add gens

--- packages/network/dist/config/tokens/crosschain/genshiro.d.ts
+++ packages/network/dist/config/tokens/crosschain/genshiro.d.ts
@@ -1,6 +1,7 @@
 import type { CrosschainTokenConf } from "../../../types/v1";
 import type { GenshiroContext } from "../../chains/genshiro";
 export declare const eqd: CrosschainTokenConf<GenshiroContext>;
+export declare const gens: CrosschainTokenConf<GenshiroContext>;
 export declare const xdot: CrosschainTokenConf<GenshiroContext>;
 export declare const xdot2: CrosschainTokenConf<GenshiroContext>;
 export declare const xdot3: CrosschainTokenConf<GenshiroContext>;
